9758b812e6dd2ba9679d55784b5b005ef4a33a8a
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-16 23:42:22 -0300

    acento nas palavras e ajuste de sequencia de perguntas.

--- TipoCestas.java
+++ TipoCestas.java
@@ -20,10 +20,10 @@ public class TipoCestas extends PessoaController {
 		tipoCestas.add(tipoCesta4);
 		tipoCestas.add(tipoCesta5);
 		
-		Thread.sleep(2400);
+		Thread.sleep(2000);
 		System.out.println("\nVocê gostaria de mudar alguma Cesta? Caso selecione NÃO, "
 				+ "a cesta escolhida será a padrão.");
-		Thread.sleep(2400);
+		Thread.sleep(3000);
 		System.out.println();
 		
 		// Loop para imprimir itens do array de cestas.
@@ -31,8 +31,8 @@ public class TipoCestas extends PessoaController {
 			System.out.println((i+1)+". "+tipoCestas.get(i));
 			}
 		
-		Thread.sleep(2000);
-		System.out.println("\nDigite [1] para SIM ou [2] para NÃO. ");
+		Thread.sleep(2400);
+		System.out.println("\nDigite [1] para SIM ou [2] para NÃO.");
 		
 		// Variável n recebe valor se o usuário quer trocar cesta ou nao.
 		int n = leia.nextInt(); 
@@ -53,7 +53,7 @@ public class TipoCestas extends PessoaController {
 					
 					// Ler número da cesta para troca.
 					int c = leia.nextInt(); 
-					System.out.println("Sua Cesta e do tipo "+tipoCestas.get(c)+".");
+					System.out.println("Sua Cesta é do tipo "+tipoCestas.get(c)+".");
 					
 					// Adiciona no array "cestas do pedido" a cesta escolhida pelo usuário.
 					cestasDoPedido.add(tipoCestas.get(c));
@@ -65,14 +65,14 @@ public class TipoCestas extends PessoaController {
 					System.out.println("Quantas cestas gostaria de trocar? ");
 					numeroDeTrocas = leia.nextInt();
 					System.out.println();
-					
-					System.out.println("Digite o número da Cesta que deseja trocar: ");
-					System.out.println("Opções de cestas: ");
+					System.out.println("\nOpções de cestas: ");
 					
 					// Loop para imprimir itens do array de cestas.
 					for (int i=1; i<tipoCestas.size(); i++) {  // .size retorna quantidade de itens do array tiposCestas.
 						System.out.println((i)+". "+tipoCestas.get(i));
 					}
+					
+					System.out.println("Digite o número da Cesta que deseja trocar: ");
 					
 					// Insere número que deseja trocar.
 					int c = leia.nextInt(); 				// 0 = posicao cesta comum.	 // c = cesta escolhida pelo usuário.
@@ -84,7 +84,7 @@ public class TipoCestas extends PessoaController {
 					// Loop para continuar a troca das cestas -> -1 pois uma cesta ja foi adicionada acima.
 					for(int f=quantidadeCesta - 1; f>(quantidadeCesta - numeroDeTrocas); f--) {
 						System.out.println();
-						System.out.println("Digite a PROXIMA cesta que deseja trocar: ");
+						System.out.println("Digite a PRÓXIMA cesta que deseja trocar: ");
 						
 						// Insere número da cesta que deseja trocar.
 						int g = leia.nextInt(); 			       // 0 = posicao cesta comum.	   // g = cesta escolhida pelo usuário.
@@ -93,7 +93,7 @@ public class TipoCestas extends PessoaController {
 						System.out.println();
 					}
 					System.out.println();
-					System.out.println("Pre cadastro realizado com sucesso! Por favor envie a documentacao necessaria.");
+					System.out.println("Pré cadastro realizado com sucesso! Por favor envie a documentação necessária.");
 				}
 				else {
 					System.out.println("Digite um valor válido!");
@@ -108,7 +108,7 @@ public class TipoCestas extends PessoaController {
 		
 		// n igual a 2 -> nao quer trocar de cesta e usuário tem apenas 1 cesta.
 		else if (n == 2 && quantidadeCesta == 1) {
-			System.out.println("Sua Cesta e do tipo "+tipoCestas.get(0)+".");
+			System.out.println("Sua Cesta é do tipo "+tipoCestas.get(0)+".");
 		}
 		
 		// Caso digite numero diferente de [1]sim ou [2]nao.
